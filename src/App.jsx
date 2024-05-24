47ca449713b09aeabba52e565eb61bd9db217b25
ThimalUshara <thimalushara@gmail.com>
2024-05-24 18:42:55 +0530

    created components for cards, homeCard joblistings, joblist and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+      <Hero />
 
-export default App
+      <HomeCards />
+
+      <JobListings />
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </div>
+  );
+};
+
+export default App;
